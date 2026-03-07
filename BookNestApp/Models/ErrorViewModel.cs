bb6a8bae660cb3ab2b25da37becee57a65fc67f0
BhuvaneswaranMurugesan <bhuvaneswaran.kanini@gmail.com>
2026-03-08 00:10:58 +0530

    [App] - Initial Commit

---
+++ BookNestApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookNestApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
